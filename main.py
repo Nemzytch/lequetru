1787508461f9c5361bd658f2592ad366bad6971f
Nemzytch <lesieutre.kevin@gmail.com>
2021-08-09 06:01:56 +0200

    splitted import

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-import os
-import time
-import json
-import math
-import random
-import win32api
-import win32con
-import requests
-import pyautogui
-import pyscreeze
-import pytesseract
-import numpy as nm
-import pydirectinput
-from time import sleep
-from PIL import ImageGrab
-from random import randrange
-from requests.adapters import HTTPAdapter
-from requests.exceptions import HTTPError
-from requests.exceptions import ConnectionError
-from requests.packages.urllib3.util.retry import Retry
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import splitimport
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning) #( ´･ω･)人(・ω・｀ )   
 os.system("")
--- splitimport.py
+++ splitimport.py
@@ -1,6 +1,21 @@
+import os
 import time
-
-
-a = time.time()
-b = a*2
-print(a)
+import json
+import math
+import random
+import win32api
+import win32con
+import requests
+import pyautogui
+import pyscreeze
+import pytesseract
+import numpy as nm
+import pydirectinput
+from time import sleep
+from PIL import ImageGrab
+from random import randrange
+from requests.adapters import HTTPAdapter
+from requests.exceptions import HTTPError
+from requests.exceptions import ConnectionError
+from requests.packages.urllib3.util.retry import Retry
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
