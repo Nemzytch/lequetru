0e56af3bb3c4a89912ab85b5f55d695c10069cbf
youssef <youssefachour12@live.com>
2022-01-23 21:49:57 +0100

    Update start

--- Start.py
+++ Start.py
@@ -1,10 +1,16 @@
 from subprocess import run
 from time import sleep
+import subprocess
+
 
 # Path and name to the script you are trying to start
 file_path = "Main.py" 
-
 restart_timer = 2
+
+subprocess.call(["git", "reset", "--hard", "HEAD"])
+subprocess.call(["git", "pull"])
+print('Updating')
+
 def start_script():
     try:
         # Make sure 'python' command is available
