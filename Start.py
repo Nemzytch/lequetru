02cfe6927644cc0e3b6ed32d078807cb2cf1b47b
youssef <youssefachour12@live.com>
2022-04-13 23:20:04 +0200

    redemarage lol

--- Start.py
+++ Start.py
@@ -1,11 +1,13 @@
+import imp
 from subprocess import run
 from time import sleep
 import subprocess
-
+import os
+import time
 
 # Path and name to the script you are trying to start
 file_path = "Main.py" 
-restart_timer = 2
+restart_timer = 10
 
 subprocess.call(["git", "reset", "--hard", "HEAD"])
 subprocess.call(["git", "pull"])
@@ -17,11 +19,20 @@ def start_script():
         run("python "+file_path, check=True)
     except:
         # Script crashed, lets restart it!
-        handle_crash()
+        try:
+            os.system('taskkill /f /im "LeagueClient.exe"')
+            os.system('taskkill /f /im "League of Legends.exe"')
+            
+            time.sleep(10)
+            
+            os.startfile("C:\Riot Games\League of Legends\LeagueClient.exe")
+            handle_crash()
+        except:
+            print("Could not restart script")
     
 
 def handle_crash():
-    sleep(restart_timer)  # Restarts the script after 2 seconds
+    sleep(restart_timer)  # Restarts the script after 10 seconds
     start_script()
 
 start_script()
