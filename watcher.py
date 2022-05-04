81d2a0540caa6f296b1c1fbddfe1d5a6e7ef31eb
Nemzytch <lesieutre.kevin@gmail.com>
2022-05-04 16:45:40 +0200

    watcher

---
+++ watcher.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import time
+import psutil
+
+
+for proc in psutil.process_iter():
+    print(proc)
+
+    #find league of legends , or riot games in process list
+
+
+#list all running process
+
+
+
+
+
