ef7bb8a4e552918cb35574645244faeae50f93c5
Nemzytch <lesieutre.kevin@gmail.com>
2022-05-04 16:51:51 +0200

    azfazef

--- watcher.py
+++ watcher.py
@@ -8,6 +8,13 @@ for proc in psutil.process_iter():
     print(proc)
     if proc.name() == "RiotClientUxRender.exe":
         print("found riot client ux render.exe")
+    if proc.name() == "RiotClientUx.exe":
+        print("found riot client ux.exe")
+    if proc.name() == "RiotClient.exe":
+        print("found riot client.exe")
+    if proc.name() == "RiotClientCrashHandler.exe":
+        print("found riot client crash handler.exe")
+    
 
     #find league of legends , or riot games in process list
 
