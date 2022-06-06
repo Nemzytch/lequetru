bc54b1c46c48376ca5d739c873616c8f6cef5718
Nemzytch <lesieutre.kevin@gmail.com>
2022-06-06 18:18:18 +0200

    client unnerf

--- cpu.bat
+++ cpu.bat
@@ -1,3 +1,3 @@
 @echo off 
-start bes.exe -J "C:\Riot Games\League of Legends\Game\League of Legends.exe" 65 -m "C:\Riot Games\League of Legends\LeagueClientUxRender.exe" 80 -m 
+start bes.exe -J "C:\Riot Games\League of Legends\Game\League of Legends.exe" 65 -m "C:\Riot Games\League of Legends\LeagueClientUxRender.exe" 65 -m 
 exit
