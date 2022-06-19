4c9cd0aaf8de3be4360343a5e0db589deccb2521
youssef <youssefachour12@live.com>
2022-06-19 18:24:18 +0200

    5

--- main.py
+++ main.py
@@ -539,12 +539,12 @@ def Connexion():  # sourcery skip: low-code-quality
             listOfAcc = table.all(formula=formula)
             
             print("Number of acc for the HWID : " + str(len(listOfAcc)))
-            if len(listOfAcc) <2:
+            if len(listOfAcc) <5:
                 print("You need more accounts")
-                for i in range(2-len(listOfAcc)):
+                for i in range(5-len(listOfAcc)):
                     print("Adding account")
                     table.update(listOfNone[i]['id'], {"HWID": hwid})
-            if len(listOfAcc) >= 2:
+            if len(listOfAcc) >= 5:
                 print("You have enough accounts")
                 Personnage.account = table.first(formula=formula, sort=["Unban"])['fields']['Account']
     
