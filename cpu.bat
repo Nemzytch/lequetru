1dc8266126f212a63b189ae6b93beaca8ac5bf63
youssef <youssefachour12@live.com>
2022-06-26 01:07:16 +0200

    50

--- cpu.bat
+++ cpu.bat
@@ -1,3 +1,3 @@
 @echo off 
-start bes.exe -J "C:\Riot Games\League of Legends\Game\League of Legends.exe" 70 -m "C:\Riot Games\League of Legends\LeagueClientUxRender.exe" 65 -m 
+start bes.exe -J "C:\Riot Games\League of Legends\Game\League of Legends.exe" 50 -m "C:\Riot Games\League of Legends\LeagueClientUxRender.exe" 65 -m 
 exit
