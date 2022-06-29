1d1d4dc09dd74a74da9c9a8c634d12a19b1dce31
youssef <youssefachour12@live.com>
2022-06-29 19:27:19 +0200

    S

--- Ignition.py
+++ Ignition.py
@@ -3,14 +3,6 @@ import subprocess
 import os
 import time
 import mouse 
-import win32gui
-import ctypes
-
-
-#__________ SCREEN ELEMENT __________________
-user32 = ctypes.windll.user32
-screenWidth = user32.GetSystemMetrics(0)
-screenHeight = user32.GetSystemMetrics(1)
 
 os.startfile("C:\\Riot Games\\League of Legends\\LeagueClient.exe")
 print("Starting League of Legends..")
@@ -19,19 +11,6 @@ time.sleep(15)
 file_path = "C:\\Users\\Administrator\\Desktop\\lequetru\\start.py"
 directory = "C:\\Users\\Administrator\\Desktop\\lequetru"
 subprocess.Popen(["start", "cmd", "/k", file_path], shell = True, cwd = directory)
-time.sleep(3)
-
-xpos = 0
-ypos = 400
-width = 400
-length = 400
-
-def enumHandler(hwnd, lParam):
-    #thetitle = win32gui.GetWindowText(hwnd)
-    if win32gui.IsWindowVisible(hwnd):
-        if 'cmd.exe' in win32gui.GetWindowText(hwnd):
-            win32gui.MoveWindow(hwnd, xpos, ypos, width, length, True)
 
 
-win32gui.EnumWindows(enumHandler, None)
 
--- Start.py
+++ Start.py
@@ -4,6 +4,28 @@ from time import sleep
 import subprocess
 import os
 import time
+import win32gui
+import ctypes
+
+
+#__________ SCREEN ELEMENT RESIZE__________________
+user32 = ctypes.windll.user32
+screenWidth = user32.GetSystemMetrics(0)
+screenHeight = user32.GetSystemMetrics(1)
+
+xpos = 0
+ypos = 400
+width = 400
+length = 400
+
+def enumHandler(hwnd, lParam):
+    #thetitle = win32gui.GetWindowText(hwnd)
+    if win32gui.IsWindowVisible(hwnd):
+        if 'cmd.exe' in win32gui.GetWindowText(hwnd):
+            win32gui.MoveWindow(hwnd, xpos, ypos, width, length, True)
+
+
+win32gui.EnumWindows(enumHandler, None)
 
 # Path and name to the script you are trying to start
 file_path = "Main.py" 
