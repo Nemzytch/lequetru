9a46f2439b20ade6b0ee13e4fd233af2ad707464
youssef <youssefachour12@live.com>
2022-06-29 18:05:31 +0200

    igniton

--- Ignition.py
+++ Ignition.py
@@ -2,12 +2,12 @@ import subprocess
 import os
 import time
 
-file_path = "start.py" 
+file_path = "C:\\Users\\Administrator\\Desktop\\lequetru"
 
 time.sleep(10)
 os.startfile("C:\\Riot Games\\League of Legends\\LeagueClient.exe")
 print("Starting League of Legends..")
-time.sleep(100)
+time.sleep(30)
 
 
 subprocess.Popen(["start", "cmd", "/k", file_path], shell = True)
