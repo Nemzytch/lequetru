b8e1ecaa1a51065937e886937420abfe9f5ea31f
youssef <youssefachour12@live.com>
2022-07-28 20:42:30 +0200

    d

--- tableActions.py
+++ tableActions.py
@@ -26,36 +26,43 @@ table2 = Table(API_KEY, 'appHnr7cu8j1HlMC2', 'ADMIN')
 
 def get_logins():
     list_of_account = []
-    list_of_finished = []
-    list_of_banned = []
-    list_of_connected = []
+    list_of_not_available = []
+    list_of_available = []
     
     for records in table.all():
         list_of_account.append(records['fields']['Account'])
         
-        if records['fields']['FinishedAcc'] == "Finish":
-            list_of_finished.append(records['fields']['Account'])
+        if records['fields']['FinishedAcc'] == "Finish" :
+            list_of_not_available.append(records['fields']['Account'])
             
         if records['fields']['FinishedAcc'] == "Banned ?":
-            list_of_banned.append(records['fields']['Account'])
+            list_of_not_available.append(records['fields']['Account'])
+            
+        if records['fields']['FinishedAcc'] == "En Vente":
+            list_of_not_available.append(records['fields']['Account'])
+        
+        if records['fields']['FinishedAcc'] == "Vendu":
+            list_of_not_available.append(records['fields']['Account'])
             
     for records in table2.all():
-        list_of_connected.append(records['fields']['ConnectedOn'])
+        list_of_not_available.append(records['fields']['ConnectedOn'])
     
-    for i in range(5):
-        for element in list_of_account:
-            # if element in list_of_connected or element in list_of_finished or element in list_of_banned:
-            if element in list_of_connected or element in list_of_banned:
-                list_of_account.remove(element)
+    print("Accounts : " + str(len(list_of_account)))
+    
+    for element in list_of_account:
+        if element not in list_of_not_available:
+            list_of_available.append(element)
+    
+    print("Available : " + str(len(list_of_available)))
 
     for records in table.all(sort=["Unban"]):
-        for element in list_of_account:
+        for element in list_of_available:
             if records['fields']['Account'] == element:
                 recordId = records['id']
                 Time = requests.get("http://worldtimeapi.org/api/timezone/Europe/Paris").json()['utc_datetime']
                 table.update(recordId, {"Unban": Time})
                 return records['fields']['Account'], records['fields']['Password']
-    
+
 def update_admin(login):
     try:
         for records in table2.all():
